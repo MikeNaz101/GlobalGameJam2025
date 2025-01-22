4fc76eb6eccda7bb0be926f590651dd5575f1f5b
MikeNaz101 <mike.yamaha.naz@gmail.com>
2025-01-21 22:46:42 -0500

    Made a status bar...
    Doesn't currently work, but I'm getting there!

---
+++ GlobalGameJam25/Assets/MikeScripts/ManaProgressBar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode()]
+public class ManaProgressBar : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/UI/Linear Progress Bar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("UI/Linear Progress Bar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+
+    public int maximum;
+    public int minimum;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Color color;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+
+        fill.color = color;
+    }
+}
